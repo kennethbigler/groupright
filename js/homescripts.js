ab4bce00237c403dab7cf0e1b3077c8c3b1a8af6
zwilson7 <zwilson7@gmail.com>
2014-12-14 14:01:25 -0800

    Bootstrapping the Boiler Plate Home Page

--- js/homescripts.js
+++ js/homescripts.js
@@ -13,3 +13,41 @@ window.onload = function(){
 		document.body.style.backgroundColor = "#04518C";
 	}
 }
+function addEmail(){
+	var email=document.getElementById("inputEmail").value;
+	/*****************************************************
+	//Validate Email Address Before Continuing
+	*****************************************************/
+    if (email == null || email == "") {
+        alert("You must enter an email address");
+        return false;
+    }
+    else{
+	    var atpos = email.indexOf("@");
+	    var dotpos = email.lastIndexOf(".");
+	    if (atpos< 1 || dotpos<atpos+2 || dotpos+2>=email.length) {
+	        alert("Not a valid e-mail address");
+	        return false;
+	    }
+	}
+	/*****************************************************
+	//Send AJAX Request to Server
+	*****************************************************/
+	var xhr;  
+	if (window.XMLHttpRequest) { // Mozilla, Safari, ...  
+	    xhr = new XMLHttpRequest();  
+	} else if (window.ActiveXObject) { // IE 8 and older  
+	    xhr = new ActiveXObject("Microsoft.XMLHTTP");  
+	} 
+	xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200){
+            //alert(xhr.responseText); // Here is the response
+        }
+    }
+	var data="email_address="+email;
+	//alert(data);
+	xhr.open("POST", "mailing-list.php", true); 
+	xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");                    
+    xhr.send(data);
+ }
+
