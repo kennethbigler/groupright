17f525f49d84efd8b8d3c0a4f6bcaa68ce9c8ab7
kennethbigler <kennethbigler@gmail.com>
2014-12-13 17:04:48 -0800

    fixed non-support for IE8 and earlier

--- js/homescripts.js
+++ js/homescripts.js
@@ -3,12 +3,14 @@ window.onload = function(){
 	if (document.all && !document.querySelector) {
 		document.getElementById('hidethis').style.display='none';
 		document.getElementById('mywarning').style.display='block';
+		document.body.style.backgroundColor = "#04518C";
 	}
 
 	//check for IE8
 	if (document.all && document.querySelector && !document.addEventListener) {
 		document.getElementById('hidethis').style.display='none';
 		document.getElementById('mywarning').style.display='block';
+		document.body.style.backgroundColor = "#04518C";
 	}
 
 	//all other browsers
