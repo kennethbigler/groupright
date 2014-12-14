474bc8cd232a80fe8289c7481f64b0c44c0e2a27
kennethbigler <kennethbigler@gmail.com>
2014-12-13 16:39:37 -0800

    working on IE 8 hides, incomplete, to be done soon

--- js/homescripts.js
+++ js/homescripts.js
@@ -1,5 +1,18 @@
-window.onload=fuction=(){
+window.onload = function(){
+	//check for IE7 or lower
+	if (document.all && !document.querySelector) {
+		document.getElementById('hidethis').style.display='none';
+		document.getElementById('mywarning').style.display='block';
+	}
 
+	//check for IE8
+	if (document.all && document.querySelector && !document.addEventListener) {
+		document.getElementById('hidethis').style.display='none';
+		document.getElementById('mywarning').style.display='block';
+	}
 
-
+	//all other browsers
+	else {
+		alert("#420YOLOSWAG4Jesus");
+	}
 }
