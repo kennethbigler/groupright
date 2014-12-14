f2dd40afe41eb2f2e17f8fc09698d468ed7e62e5
scott-m-sarsfield <scott.m.sarsfield@gmail.com>
2014-12-13 23:01:09 -0800

    Login (Start) + Fleshed 'dev'

---
+++ dev/js/homescripts.js
@@ -0,0 +1,15 @@
+window.onload = function(){
+	//check for IE7 or lower
+	if (document.all && !document.querySelector) {
+		document.getElementById('hidethis').style.display='none';
+		document.getElementById('mywarning').style.display='block';
+		document.body.style.backgroundColor = "#04518C";
+	}
+
+	//check for IE8
+	if (document.all && document.querySelector && !document.addEventListener) {
+		document.getElementById('hidethis').style.display='none';
+		document.getElementById('mywarning').style.display='block';
+		document.body.style.backgroundColor = "#04518C";
+	}
+}
