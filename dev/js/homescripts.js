17cd5485360e7a21afc6076a143f1249a136b69d
zwilson7 <zwilson7@gmail.com>
2014-12-20 23:17:58 -0800

    Updated home dropdown, skeletoned js for logout

--- dev/js/homescripts.js
+++ dev/js/homescripts.js
@@ -1,3 +1,4 @@
+var visibleDropdown=false;
 window.onload = function(){
 	//check for IE7 or lower
 	if (document.all && !document.querySelector) {
@@ -12,4 +13,41 @@ window.onload = function(){
 		document.getElementById('mywarning').style.display='block';
 		document.body.style.backgroundColor = "#04518C";
 	}
+}
+function showDropdown(){
+	if(visibleDropdown){
+		document.getElementById('dropdown').style.display='none';
+		document.getElementById('arrow').className="glyphicon glyphicon-chevron-down";
+	}else{
+		document.getElementById('dropdown').style.display='block';
+		document.getElementById('arrow').className="glyphicon glyphicon-chevron-up";
+	}
+	visibleDropdown=!visibleDropdown;
+}
+function logoutAndRedirect(){
+	
+	var _cookies = genCookieDictionary();
+
+	if(_cookies.accesscode && _cookies.user){
+	
+		var obj = {
+			"code":_cookies.accesscode,
+			"email":_cookies.user,
+			"function":"logout_user"
+		};
+	
+		// Contact Server
+		$.ajax("https://www.groupright.net/dev/groupserve.php",{
+			type:"POST",
+			data:obj,
+			statusCode:{
+				200: function(){
+						window.location = "./index.html"; /* presumed redirect */
+					},
+				211: /*Figure out what to do*/
+			}
+		
+		});
+	}
+	
 }
