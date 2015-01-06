1a4fa9477e0073a3b3e76a38ee17274717d13d72
zwilson7 <zwilson7@gmail.com>
2015-01-05 21:57:15 -0800

    Dynamic Home Loading

--- dev/js/homescripts.js
+++ dev/js/homescripts.js
@@ -33,33 +33,74 @@ function logoutAndRedirect(){
 	*/
 }
 
-var text = '{"groups":[' +
-	'{"groupName":"Senior Design","groupColor":"red"},' +
-	'{"groupName":"Web Dev","groupColor":"orange"},' +
-	'{"groupName":"Family","groupColor":"yellow"},' +
-	'{"groupName":"Fake Madrid","groupColor":"green"},' +
-	'{"groupName":"Spiked Punch","groupColor":"blue"},' +
-	'{"groupName":"My Villa","groupColor":"purple"}]}';
+function addUsersInfo(data){
+	obj = JSON.parse(data);
+	//console.log(obj);
+	//console.log(obj.memberships);
+	initials=obj.first_name[0] + obj.last_name[0];
+	addUsersGroups(obj.memberships);
+	dealwithProfilePic(obj.photo_url,initials);
+	addUsersName(obj.first_name);
+}
+function addUsersName(firstName){
+	document.getElementById("profileName").innerHTML=firstName+'<span class="caret"></span>';
+}
 
-obj = JSON.parse(text);
-
-alert(obj);
-console.log(obj);
-console.log(obj.groups);
+function dealwithProfilePic(url, initials){
+	//alert(initials);
+	var profileImage=document.getElementById("profileImage");
+	if(url==null){
+		//alert("here");
+		profileImage.src="images/black.png";
+		profileImage.style.backgroundColor="orange";
+		profileImage.style.border="2px solid #AF7817";
+		//still have to do something with initials
+		return;
+	}
+	profileImage.src=url;
+}
 
 /* Populate the groups field on the homepage with the "groups" json object */
-window.onload = function() {
+function addUsersGroups(allGroups){
+	
 	var groupMenu = document.getElementById("myGroups");
-	var numGroups = obj.groups.length;
-	alert(numGroups);
+	var numGroups = allGroups.length;
 	var allMyGroups = '';
 
 	for(var i = 0; i < numGroups; i ++) {
 		allMyGroups += '<li><a href="#"><span class="glyphicon glyphicon-stop" style="color:' + 
-			obj.groups[i].groupColor +
+			allGroups[i].groupColor +
 			';"></span>&nbsp;' +
-			obj.groups[i].groupName +
+			allGroups[i].groupName +
 			'</a></li>';
 	}
 	groupMenu.innerHTML = allMyGroups;
+}
+window.onload = function() {
+	//get the cookies and get all of the data from the server
+	var _cookies = genCookieDictionary();
+
+	if(_cookies.accesscode && _cookies.user){
+	
+		var obj = {
+			"cookie":_cookies.accesscode,
+			"email":_cookies.user,
+			"function":"get_user_info"
+		};
+	
+		// Contact Server
+		$.ajax("https://www.groupright.net/dev/groupserve.php",{
+			type:"POST",
+			data:obj,
+			statusCode:{
+				200: function(data, status, jqXHR){
+					addUsersInfo(data);
+				}
+			}
+		
+		});
+	}
+
+	
+	
 };
