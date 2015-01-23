4d41567920162147a2880ef2784e2097e584ade8
scott-m-sarsfield <scott-m-sarsfield>
2015-01-23 14:26:15 -0800

    Server!

---
+++ dev/server/groups.php
@@ -0,0 +1,183 @@
+<?php
+
+/**
+ *	GROUPS.PHP
+ *
+ *		Handles much of the logic pertaining to generic group tasks (creation, deletion, etc.)
+ */
+ 
+	function makeGroup_Helper($name,$members,$leader){
+	
+	/*
+	INSERT INTO `groups`(`group_name`, `group_leader`, `properties`) VALUES ('My Family','zwilson7@gmail.com','{"color":"#990000"}')
+	*/
+	
+		// Enter DB.
+		$dbh = ConnectToDB();
+	
+		// Prevent duplicate.
+		$stmt = $dbh->prepare(
+			"SELECT * FROM `groups` where group_name=? AND group_leader=?"
+		);				
+		$stmt->execute(array($name,$leader));			
+		while($row = $stmt->fetch()){ http_response_code(213); return; }
+	
+	
+		// Make Group
+		$stmt = $dbh->prepare(
+			"INSERT IGNORE INTO groups(`group_name`, `group_leader`, `properties`) VALUES (?,?,'{}')"
+		);				
+		$stmt->execute(array($name,$leader));
+
+		// Get group UID.
+		$g_uid = 0;
+		$stmt = $dbh->prepare(
+			"SELECT * FROM `groups` where group_name=? AND group_leader=?"
+		);				
+		$stmt->execute(array($name,$leader));	
+		
+		while($row = $stmt->fetch()){ 
+			$g_uid = $row['group_uid'];
+		}
+		
+		// If valid id, add memberships.
+		if($g_uid != 0){
+		
+			// add leader
+			addMember($members[0],$g_uid,"leader","{\"color\":\"#900000\"}");
+			
+			// add other members
+			for($i = 1; $i < count($members); $i++){
+				addMember($members[$i],$g_uid,"member","{\"color\":\"#900000\"}");
+			}
+			
+			http_response_code(200);
+			return;
+		}
+		
+		http_response_code(220);
+		return;
+	
+	}
+ 
+	function makeGroup(){
+	
+		// Get Data
+		$email = $_POST['email'];
+		$cookie = $_POST['ac'];
+		
+		$members = json_decode($_POST['member_array']);
+		$gname = $_POST['group_name'];
+		
+		// Fix cookie.
+		$cookie = grHash($cookie,$email);
+		
+		// Filter email address
+		$email_address = htmlspecialchars($email);
+		$email_address = trim($email_address);
+		$email_address = stripslashes($email_address);
+		
+		// If valid, continue.
+		if(filter_var($email_address, FILTER_VALIDATE_EMAIL)) {
+		
+			// Enter DB.
+			$dbh = ConnectToDB();
+		
+			// Verify User.	
+			$stmt = $dbh->prepare(
+				"SELECT * FROM active_users
+				WHERE email = ? AND last_session_code = ?"
+			);				
+			$stmt->execute(array($email_address,$cookie));	
+			
+			
+			while($row = $stmt->fetch()){ 
+				makeGroup_Helper($gname,$members,$email_address);
+				return;
+			}
+			http_response_code(211);
+			return;
+			
+		}
+		else {
+			// maybe do something
+			http_response_code(220);
+			return;
+		}
+	
+	
+	}
+	
+	function addMember($email,$group_uid,$role,$properties){
+	/*
+	INSERT INTO `memberships`(`email`, `group_uid`, `role`, `properties`) VALUES ('scomatbarsar@gmail.com', 2,'member', '{"color":"#990000"}')
+	*/
+	
+		// Enter DB.
+		$dbh = ConnectToDB();
+	
+		// Prevent duplicate.
+		$stmt = $dbh->prepare(
+			"INSERT IGNORE INTO `memberships`(`email`, `group_uid`, `role`, `properties`) VALUES (?, ?, ?, ?)"
+		);				
+		$stmt->execute(array($email,$group_uid,$role,$properties));		
+		return true;
+		
+	}
+	
+	
+		/*
+		// basic
+		SELECT m.email, u.first_name, u.last_name, g.group_name, m.properties, u.last_session_code
+		FROM memberships AS m 
+		LEFT JOIN groups AS g ON m.group_uid = g.group_uid 
+		LEFT JOIN active_users AS u ON m.email = u.email
+
+		// refined
+		SELECT g.group_name, m.properties
+		FROM memberships AS m 
+		LEFT JOIN groups AS g ON m.group_uid = g.group_uid 
+		LEFT JOIN active_users AS u ON m.email = u.email
+		WHERE m.email = 'scomatbarsar@gmail.com' AND u.last_session_code = 'dd4c3357c8d031bd8119a326c006a6fd66ed029a'
+				
+		*/
+
+	function getMemberships($email_address,$cookie){
+		
+		
+		// Enter DB.
+		$dbh = ConnectToDB();
+
+		// Run Query.			
+		$stmt = $dbh->prepare(
+			"SELECT g.group_name AS group_name, m.properties AS properties
+			FROM memberships AS m 
+			LEFT JOIN groups AS g ON m.group_uid = g.group_uid 
+			LEFT JOIN active_users AS u ON m.email = u.email
+			WHERE m.email = ? AND u.last_session_code = ?"
+		);				
+		$stmt->execute(array($email_address,$cookie));	
+		
+		// Run through results
+		$groups = array();
+		while($row = $stmt->fetch()){
+		
+			// Parse rows.
+			$name = $row['group_name'];
+			$properites = json_decode($row['properties'],true);
+			
+			// Create membership object.
+			$gr = array("groupName"=>$name,"groupColor"=>$properites["color"]);
+			
+			// Add to groups.
+			$groups[] = $gr;
+		}	
+		http_response_code(200);
+			
+		
+		return $groups;		
+	}
+
+
+
+?>
---
+++ dev/server/login.php
@@ -0,0 +1,213 @@
+<?php
+
+	function incrementLoginAttempt($user){
+		
+		$dbh = ConnectToDB();
+		
+		$stmt = $dbh->prepare("SELECT * FROM active_users WHERE email=?");
+		if($stmt->execute(array($user))){
+			while($row = $stmt->fetch()){
+				$count = $row['login_attempts'] + 1;
+			}
+		}
+		
+		$stmt = $dbh->prepare("UPDATE active_users SET login_attempts=? WHERE email=?");
+		$stmt->execute(array($count,$user));
+		
+		return $count;	
+	}
+	
+	
+	function sendLockedEmail($email, $fname, $lname, $vc){
+		$hackedUrl = "https://www.groupright.net/dev/unlock.html?vc=".$vc;
+	
+		$to = $email;
+		$subject = 'Your Account Has Been Locked';
+		
+		$headers = "From: " . "no-reply@groupright.net" . "\r\n";
+		$headers .= "Reply-To: " . "no-reply@groupright.net" . "\r\n";
+		$headers .= "MIME-Version: 1.0\r\n";
+		$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+		
+		$message = '<html><body>';
+		$message .= '<img src="https://www.groupright.net/dev/images/emailLogo.png" alt="GroupRight" />';
+		$message .= '<p><strong>'.$fname." ".$lname."</strong>,</p>";
+		$message .= "<p>Someone has been trying to get into your account.  If this is not you, click the following link to unlock your account.</p>";
+		$message .= "<p><a href='".$hackedUrl."'>".$hackedUrl."</a></p>";
+		$message .= "</body></html>";
+		
+		//echo $message;
+	
+		mail($to,$subject,$message,$headers);
+	}
+	
+	
+	
+	function lockAccount($user,$vc){
+		
+		$dbh = ConnectToDB();
+		
+		$stmt = $dbh->prepare("UPDATE active_users SET last_session_code=? WHERE email=?");
+		
+		$stmt->execute(array($vc,$user));
+		
+	}
+	
+	function checkEmailPassword($user,$password){
+		
+		$dbh = ConnectToDB();
+		
+		$stmt = $dbh->prepare("SELECT * FROM active_users WHERE email=?");
+		if($stmt->execute(array($user))){
+			while($row = $stmt->fetch()){
+				if($row['login_attempts'] > 5) return false;
+				if($row['password'] == $password){
+					// Clear login attempts.
+					$stmt2 = $dbh->prepare("UPDATE active_users SET login_attempts=0 WHERE email=?");
+					$stmt2->execute(array($user));
+
+					return true;
+				}
+			}
+		}
+		
+		return false;	
+	}
+	
+	
+	function logIn(){
+	
+		// Get username and password.
+		$email = $_POST['email'];
+		$password = $_POST['password'];
+		
+		// Process password
+		$password = grHash($password,$email);
+		
+		// Filter email address
+		$email_address = htmlspecialchars($email);
+		$email_address = trim($email_address);
+		$email_address = stripslashes($email_address);
+		
+		// If valid, continue.
+		if(filter_var($email_address, FILTER_VALIDATE_EMAIL)) {
+			if(checkEmailPassword($email_address,$password)){
+				// Send success code.
+				http_response_code(200);
+				
+				// Generate and print cookie.
+				$cookieCode = generateCookie($email_address,true);
+				echo $cookieCode;
+				return;
+			
+			}
+			else{
+				// Increment try count.
+				http_response_code(206);
+				$currLogCount = incrementLoginAttempt($email_address);				
+				
+				// Locked out.
+				if($currLogCount > 5){
+					http_response_code(209);
+					
+					if($currLogCount == 6){
+					
+						// Generate verification code.
+						$vc = generateCode(32,$email_address);
+						
+						// Get name from account.
+						$arr = getNameFromAccount($email_address);
+						$fname = $arr[0];
+						$lname = $arr[1];
+						
+						// Update table to reflect lock.
+						echo $vc; echo $email_address;
+						lockAccount($email_address,$vc);
+						
+						// Send Confirmation Email
+						sendLockedEmail($email_address,$fname,$lname,$vc);
+					
+					}
+					
+				}
+			
+			}
+			
+		}
+	
+	}
+	
+	//----------------------------------------------------------------
+	function rememberUser(){
+	
+		// Get username and password.
+		$email = $_POST['email'];
+		$code = $_POST['code'];
+		
+		// Filter email address
+		$email_address = htmlspecialchars($email);
+		$email_address = trim($email_address);
+		$email_address = stripslashes($email_address);
+		
+		// If valid, continue.
+		if(filter_var($email_address, FILTER_VALIDATE_EMAIL)) {
+			if(checkCookie($email_address,$code)){
+				//echo "true";
+				http_response_code(200); return;
+			}else{
+				//echo "false";
+				http_response_code(211); return;
+			}
+			
+		}
+	
+	}
+	
+	//-----------------------------------------------------------------
+	function unlockAccount(){
+		// Get username and password.
+		$vc = $_POST['vc'];
+		
+		$dbh = ConnectToDB();
+		
+		$stmt = $dbh->prepare("SELECT * FROM active_users WHERE last_session_code=?");
+		
+		if($stmt->execute(array($vc))){
+			while($row = $stmt->fetch()){
+				$repl = generateCode(32,0);
+			
+				$stmt2 = $dbh->prepare("UPDATE active_users SET login_attempts=0, last_session_code=? WHERE last_session_code=?");
+				$stmt2->execute(array($repl,$vc));	
+				http_response_code(200);
+				return;
+			}
+		}
+		http_response_code(210);
+		return;
+		
+	}
+	
+	
+	//------------------------------------------------------------------
+	function logoutUser(){
+		
+		// Get username and password.
+		$cookie = $_POST['code'];
+		$email = $_POST['email'];
+		
+		// Filter email address
+		$email_address = htmlspecialchars($email);
+		$email_address = trim($email_address);
+		$email_address = stripslashes($email_address);
+		
+		// If valid, continue.
+		if(filter_var($email_address, FILTER_VALIDATE_EMAIL)) {
+			removeCookie($email_address,$cookie);
+			http_response_code(200);
+			return;
+		}
+		http_response_code(211);
+		return;
+	}
+	
+	?>
