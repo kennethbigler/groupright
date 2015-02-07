e9c5564f033ac9335942c67e8f84bfd83fa4ac9e
scott-m-sarsfield <scott-m-sarsfield>
2015-02-06 16:45:17 -0800

    Addition of Server Tasks and Events

---
+++ dev/server/events.php
@@ -0,0 +1,153 @@
+<?php
+function getAllEvents($email){
+	$dbh = ConnectToDB();
+	
+	$stmt = $dbh->prepare(
+		"SELECT name,description,group_uid,
+		events.email as creator_email,
+		start_time,end_time FROM events 
+		JOIN groups USING (group_uid) 
+		JOIN memberships USING(group_uid)
+		WHERE memberships.email = ?"
+	);
+	$stmt->execute(array($email));
+	
+	$arr = array();
+	
+	while($row = $stmt->fetch()){
+		$obj = array(
+			"name"=>$row['name'],
+			"description"=>$row['description'],
+			"group_id"=>$row['group_uid'],
+			"creator"=>$row['creator_email'],
+			"start_time"=>$row['start_time'],
+			"end_time"=>$row['end_time']
+		);
+		echo $obj;
+		$arr[] = $obj;
+	}
+	return $arr;
+}
+
+	function verifyUserGroup($email,$cookie,$group_uid){
+		$dbh = ConnectToDB();
+		
+		$stmt = $dbh->prepare(
+			"SELECT * FROM active_users JOIN memberships USING (email)
+			WHERE email = ?
+			AND last_session_code = ?
+			AND group_uid = ?"		
+		);
+		$stmt->execute(array($email,$cookie,$group_uid));
+		
+		while($row = $stmt->fetch()){
+			return true;
+		}
+		http_response_code(299);
+		return false;
+	}
+
+	function addEvent($email,$group_uid,$name,$description,$start_time,$end_time){
+				
+		$dbh = ConnectToDB();
+		
+		if(!isset($start_time)){
+			$stmt = $dbh->prepare(
+				"INSERT INTO events(name,description,group_uid,email,start_time,end_time)
+				VALUES(?,?,?,?,NULL,NULL)"
+			);
+			$stmt->execute(array($name,$description,$group_uid,$email));
+		}else{
+			$stmt = $dbh->prepare(
+				"INSERT INTO events(name,description,group_uid,email,start_time,end_time)
+				VALUES(?,?,?,?,?,?)"
+			);
+			$stmt->execute(array($name,$description,$group_uid,$email,$start_time,$end_time));
+		}
+		
+		return $dbh->lastInsertId();
+	}
+	
+	function addEventVoteSettings($event_uid,$start_date,$end_date,$start_time,$end_time,$duration){
+				
+		$dbh = ConnectToDB();
+		
+		$stmt = $dbh->prepare(
+			"INSERT INTO events_vote_settings(event_uid,start_date,end_date,start_time,end_time,duration)
+			VALUES(?,?,?,?,?,?);"
+		);
+		$stmt->execute(array($event_uid,$start_date,$end_date,$start_time,$end_time,$duration));
+	}
+
+	
+	function createFixedEvent(){
+	
+		// Get information.
+		$email = sanitizeEmail( $_POST['email'] );
+		$cookie = grHash($_POST['cookie'],$email);
+		$group_uid = $_POST['group_uid'];
+		$event_title = $_POST['event_title'];
+		$event_descr = $_POST['event_description'];
+		$start_time = $_POST['start_time'];
+		$end_time = $_POST['end_time'];
+		
+		
+		// If valid, continue.
+		if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
+			if(!verifyUserGroup($email,$cookie,$group_uid)) return;
+			$event_uid = addEvent($email,$group_uid,$event_title,$event_descr,$start_time,$end_time);
+		}
+	
+	}
+	
+	function createVotableEvent(){
+		
+		// Get information.
+		$email = sanitizeEmail( $_POST['email'] );
+		$cookie = grHash($_POST['cookie'],$email);
+		$group_uid = $_POST['group_uid'];
+		$event_title = $_POST['event_title'];
+		$event_descr = $_POST['event_description'];
+		
+		$start_date = $_POST['start_date'];
+		$end_date = $_POST['end_date'];
+		$start_time = $_POST['start_time'];
+		$end_time = $_POST['end_time'];
+		$duration = $_POST['duration'];
+		
+		// IF valid, continue.
+		if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+			if(!verifyUserGroup($email,$cookie,$group_uid)) return;
+			$event_uid = addEvent($email,$group_uid,$event_title,$event_descr);
+			addEventVoteSettings($event_uid,$start_date,$end_date,$start_time,$end_time,$duration);
+			addEventVotingTask($email,$group_uid,$event_title,$event_uid);
+		}
+	}
+
+	function addEventVotingTask($creator,$group_uid,$title,$event_uid){
+		
+		$task_title = "Provide Availability for '".$title."'";
+		
+		
+		$task_uid = addTask($creator,$task_title,"",$group_uid,$event_uid,true);
+		
+		// Get members.
+		$dbh = ConnectToDB();
+		
+		$stmt = $dbh->prepare(
+			"SELECT email FROM memberships WHERE group_uid = ?"
+		);
+		$stmt->execute(array($group_uid));
+				
+		while($row = $stmt->fetch()){
+			$em = $row['email'];
+			addTaskAssignment($task_uid,$group_uid,$em);
+		}
+
+	}
+
+
+
+
+
+?>
