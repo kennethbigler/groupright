4f8fe29f7209c7d1d24b5504971b0c06e36d5d74
zwilson7 <zwilson7@gmail.com>
2015-04-01 19:22:12 -0700

    Updated

--- dev/js/createTask.js
+++ dev/js/createTask.js
@@ -1,25 +1,36 @@
-function createTask(){
+function createGRTask(){
+	document.getElementById('taskError').innerHTML="";
+	//get group
+	if ($( "#taskGroups" ).val()=="XXX") {
+		document.getElementById('taskError').innerHTML="Please select a group for the task.";
+		return false;
+	}
+
 	//get the task name
 	var task_title = document.getElementById("taskNameField").value;
 	if (task_title == "" || task_title.length<=0) {
-		alert("Your Task Name is invalid");
+		document.getElementById('taskError').innerHTML="Your Task Name is invalid";
 		return false;
 	}
 
-	//get group
-	if ($( "#taskGroups" ).val()=="XXX") {
-		document.getElementById('eventError').innerHTML="Please select a group for the event.";
-		return false;
-	}
-	var taskGroup = $( "#eventGroups option:selected" ).text();
-	var taskGroupID = $( "#eventGroups" ).val();
+	
+	var taskGroup = $( "#taskGroups option:selected" ).text();
+	var taskGroupID = $( "#taskGroups" ).val();
 
 	//get personal/group status
-	var is_personal = document.getElementById("is_personal").elements["personal"].value;
+	var is_personal = $('input[name="personal"]:checked').val();
+	if (is_personal == undefined) {
+		document.getElementById('taskError').innerHTML="Please indicate if the task is personal or for the group.";
+		return false;
+	}
 
 	//get task description
 	var task_description = document.getElementById("taskDescription").value;
-	
+	if (task_description == undefined) {
+		document.getElementById('taskError').innerHTML="Your Task Name is invalid";
+		return false;
+	}
+
 	//get user access code
 	var _cookies = genCookieDictionary();
 	var email = _cookies.user; 
@@ -28,11 +39,10 @@ function createTask(){
 				"function":"create_task",
 				"email":email,
 				"cookie":ac,
-				"group_uid":,
-				"event_uid":"",
+				"group_uid":taskGroupID,
 				"task_title":task_title,
 				"task_description":task_description,
-				"is_personal":is_personal,
+				"is_personal":is_personal
 	};
 	console.log(obj);
 
@@ -42,7 +52,7 @@ function createTask(){
 			data:obj,
 			statusCode:{
 				200:function(data,status,jqXHR) {
-					alert("Group Created");
+					alert("Task Created");
 					window.location = "./home.html";				
 				},
 				210:function() {
