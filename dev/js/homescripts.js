b3659f256d5e4cb17583d908666522233957c76a
zwilson7 <zwilson7@gmail.com>
2015-04-15 12:59:52 -0700

    Major Changes to Interface for Create Group, Added buttons for lists and polls(Decisions)

--- dev/js/homescripts.js
+++ dev/js/homescripts.js
@@ -93,6 +93,7 @@ function addUsersInfo(){
 	
 	if(!initLightbox) return;
 	
+
 	// Lightbox Forms --------------------------------
 	initCreateGroup();		// 'Create a Group'
 	initScheduleEvent();	// 'Schedule an Event'
@@ -391,8 +392,33 @@ function addUpdates(){
 //-------------------------------------------------------------------
 
 function initCreateGroup(){
-	document.getElementById("member1").value = GRMAIN.user;
+	$('#selectize').selectize({
+	    plugins: ['remove_button'],
+	    valueField: 'email',
+	    labelField: 'first_name',
+	    searchField: ['first_name','email'],
+	    options: GRMAIN.contacts(),
+	    delimiter: ', ',
+	    persist: false,
+	    create: function (input) {
+	      return {
+	        email: input,
+	        first_name: input,
+	        last_name: input
+	      };
+	    },
+	    hideSelected: true,
+	    openOnFocus: false,
+	});
+
+	$('#selectize').change(function(){
+		//console.log("you select value="+$(this).val());
+		globalNewGroup=$(this).val();
+		//console.log(globalNewGroup);
+	});
+	
 }
+
 
 function initScheduleEvent(){
 	var allGroups = GRMAIN.groups();
@@ -419,8 +445,6 @@ function initScheduleEvent(){
     	todayHighlight: true,
     	autoclose:true
 	});
-	$('#timepicker1').timepicker();
-	$('#timepicker2').timepicker();
 
 	var groupMenu = document.getElementById("eventGroups");
 	var numGroups = allGroups.length;
@@ -573,49 +597,5 @@ function toggleTask(element, taskid, localIndex){
 }
 
 
-var substringMatcher = function(strs) {
-  return function findMatches(q, cb) {
-    var matches, substrRegex;
- 
-    // an array that will be populated with substring matches
-    matches = [];
- 
-    // regex used to determine if a string contains the substring `q`
-    substrRegex = new RegExp(q, 'i');
- 
-    // iterate through the pool of strings and for any string that
-    // contains the substring `q`, add it to the `matches` array
-    $.each(strs, function(i, str) {
-      if (substrRegex.test(str)) {
-        // the typeahead jQuery plugin expects suggestions to a
-        // JavaScript object, refer to typeahead docs for more info
-        matches.push({ value: str });
-      }
-    });
- 
-    cb(matches);
-  };
-};
- 
-var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
-  'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
-  'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
-  'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
-  'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
-  'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
-  'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
-  'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
-  'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
-];
- 
-$('#member2 .typeahead').typeahead({
-  hint: true,
-  highlight: true,
-  minLength: 1
-},
-{
-  name: 'states',
-  displayKey: 'value',
-  source: substringMatcher(states)
-});
+
 
