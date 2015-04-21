1f40bb69c539ddb9fdb68419f48e246f084fa878
Scott Mathias Sarsfield <scott.m.sarsfield@gmail.com>
2015-04-21 15:21:53 -0700

    iOS - Initial Storyboard

--- App/GroupRight/FirstViewController.m
+++ App/GroupRight/FirstViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "FirstViewController.h"
+#import "LoginViewController.h"
 
 @interface FirstViewController ()
 
@@ -17,6 +18,14 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    UIStoryboard *storyboard = self.storyboard;
+    
+    LoginViewController *lvc = [storyboard instantiateViewControllerWithIdentifier:@"loginVC"];
+    [self presentViewController:lvc animated:NO completion:nil];
 }
 
 - (void)didReceiveMemoryWarning {
